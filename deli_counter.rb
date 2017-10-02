35b19d19b512df3e5500030b1a05e22b9b58cf40
Elisa Vaccaro <erv230@nyu.edu>
2017-10-02 23:16:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(list)
   else
     list.each do |person|
       lines += "#{list.index(person)+1}. #{person} "
-      lines.slice(0, -1)
+      lines.slice!(0, -1)
   end
   puts "The line is currently: #{lines}"
 end
