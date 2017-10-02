7bd188bfd6fabf5932257adeca32edacdf7badba
Elisa Vaccaro <erv230@nyu.edu>
2017-10-02 22:42:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@ def line(list)
   if list.length == 0
     puts "The line is currently empty."
   else
-    lines = " "
     list.each do |person|
+      lines = " "
       lines += `#{list.index(person)+1}. #{person}"`
   end
   puts "The line is currently: #{lines}"
