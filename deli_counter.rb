3eb90a0d975cfc9a9adf745b09977f455493ef01
Elisa Vaccaro <erv230@nyu.edu>
2017-10-02 23:50:10 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,10 @@ def take_a_number(line, person)
   line.push(person)
   puts "Welcome, #{person}. You are number #{line.index(person)+1} in line."
 end
+
+def now_serving(line)
+  if line.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line.pop}."
+  end
